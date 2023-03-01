b22aec1fb4c1c99a0896da94f5b8af3a54358f11
Artem Romanov <a.romanov@unabank.com>
2023-03-01 18:39:15 +0400

    Initial Commit

---
+++ sui/suiApp.swift
@@ -0,0 +1,17 @@
+//
+//  suiApp.swift
+//  sui
+//
+//  Created by Артем Романов on 01.03.2023.
+//
+
+import SwiftUI
+
+@main
+struct suiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
